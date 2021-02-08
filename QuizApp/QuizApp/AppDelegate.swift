b7272b87bf9ed30b32920d3f6467a73da874dfd7
Clay Suttner <csuttner@gmail.com>
2021-02-08 14:10:48 -0800

    tests callbacks for single and multiple selections in tableView

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -13,7 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = QuestionViewController(question: "A question?", options: ["Option 1", "Option 2"]) {
+            print($0)
+        }
+        _ = viewController.view
+        viewController.tableView.allowsMultipleSelection = true
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
         return true
     }
 
