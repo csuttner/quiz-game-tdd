790408efebfa3ad243e3b9cc9498c42eab41c615
Clay Suttner <csuttner@gmail.com>
2021-02-09 09:46:47 -0800

    adds previously untracked files

---
+++ QuizApp/QuizApp/ResultsViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ResultsViewController.swift
+//  QuizApp
+//
+//  Created by Clay Suttner on 2/8/21.
+//
+
+import Foundation
+import UIKit
+
+struct PresentableAnswer {
+    let question: String
+    let answer: String
+    let isCorrect: Bool
+}
+
+class CorrectAnswerCell: UITableViewCell {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+}
+
+class WrongAnswerCell: UITableViewCell {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var correctAnswerLabel: UILabel!
+    @IBOutlet weak var wrongAnswerLabel: UILabel!
+}
+
+class ResultsViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var summary = ""
+    private var answers = [PresentableAnswer]()
+    
+    convenience init(summary: String, answers: [PresentableAnswer]) {
+        self.init()
+        self.summary = summary
+        self.answers = answers
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headerLabel.text = summary
+        tableView.register(UINib(nibName: "CorrectAnswerCell", bundle: nil), forCellReuseIdentifier: "CorrectAnswerCell")
+        tableView.register(UINib(nibName: "WrongAnswerCell", bundle: nil), forCellReuseIdentifier: "WrongAnswerCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let answer = answers[indexPath.row]
+        if answer.isCorrect {
+            return correctAnswerCell(for: answer)
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WrongAnswerCell") as! WrongAnswerCell
+        cell.questionLabel.text = answer.question
+        cell.correctAnswerLabel.text = answer.answer
+        return cell
+    }
+    
+    private func correctAnswerCell(for answer: PresentableAnswer) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "CorrectAnswerCell") as! CorrectAnswerCell
+        cell.questionLabel.text = answer.question
+        cell.answerLabel.text = answer.answer
+        return cell
+    }
+}
---
+++ QuizApp/QuizAppTests/ResultsViewControllerTest.swift
@@ -0,0 +1,77 @@
+//
+//  ResultsViewControllerTest.swift
+//  QuizAppTests
+//
+//  Created by Clay Suttner on 2/8/21.
+//
+
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class ResultsViewControllerTest: XCTestCase {
+    
+    func test_viewDidLoad_renderSummary() {
+        XCTAssertEqual(makeSUT(summary: "a summary").headerLabel.text, "a summary")
+    }
+    
+    func test_viewDidLoad_rendersAppropriateNumberOfAnswers() {
+        XCTAssertEqual(makeSUT(answers: []).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(answers: [makeDummyAnswer()]).tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    func test_viewDidLoad_withCorrectAnswer_renderCorrectAnswerCell() {
+        let sut = makeSUT(answers: [makeAnswer(isCorrect: true)])
+
+        let cell = sut.tableView.cell(at: 0) as? CorrectAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+    
+    func test_viewDidLoad_withCorrectAnswer_configuresCell() {
+        let answer = makeAnswer(question: "Q1", answer: "A1", isCorrect: true)
+        let sut = makeSUT(answers: [answer])
+
+        let cell = sut.tableView.cell(at: 0) as? CorrectAnswerCell
+        
+        XCTAssertNotNil(cell)
+        XCTAssertEqual(cell?.questionLabel.text, "Q1")
+        XCTAssertEqual(cell?.answerLabel.text, "A1")
+    }
+    
+    func test_viewDidLoad_withWrongAnswer_renderWrongAnswerCell() {
+        let sut = makeSUT(answers: [makeAnswer(isCorrect: false)])
+ 
+        let cell = sut.tableView.cell(at: 0 ) as? WrongAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+    
+    func test_viewDidLoad_withWrongAnswer_configuresCell() {
+        let answer = makeAnswer(question: "Q1", answer: "A1", isCorrect: false)
+        let sut = makeSUT(answers: [answer])
+
+        let cell = sut.tableView.cell(at: 0) as? WrongAnswerCell
+        
+        XCTAssertNotNil(cell)
+        XCTAssertEqual(cell?.questionLabel.text, "Q1")
+        XCTAssertEqual(cell?.correctAnswerLabel.text, "A1")
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT(summary: String = "", answers: [PresentableAnswer] = []) -> ResultsViewController {
+        let sut = ResultsViewController(summary: summary, answers: answers)
+        _ = sut.view
+        return sut
+    }
+    
+    func makeDummyAnswer() -> PresentableAnswer {
+        return makeAnswer(isCorrect: false)
+    }
+    
+    func makeAnswer(question: String = "", answer: String = "", isCorrect: Bool) -> PresentableAnswer {
+        return PresentableAnswer(question: question, answer: answer, isCorrect: isCorrect)
+    }
+    
+}
