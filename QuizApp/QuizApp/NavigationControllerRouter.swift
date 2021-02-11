bb333ad898d33aa659bc6b3b876bc12cf3f80012
Clay Suttner <csuttner@gmail.com>
2021-02-11 12:04:26 -0800

    adds question enum

--- QuizApp/QuizApp/NavigationControllerRouter.swift
+++ QuizApp/QuizApp/NavigationControllerRouter.swift
@@ -8,18 +8,25 @@
 import UIKit
 import QuizEngine
 
+protocol ViewControllerFactory {
+    func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController
+}
+
 class NavigationControllerRouter: Router {
     typealias Question = String
     typealias Answer = String
     
     private let navigationController: UINavigationController
+    private let factory: ViewControllerFactory
     
-    init(_ navigationController: UINavigationController) {
+    init(_ navigationController: UINavigationController, factory: ViewControllerFactory) {
         self.navigationController = navigationController
+        self.factory = factory
     }
     
     func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
-        navigationController.pushViewController(UIViewController(), animated: false)
+        let viewController = factory.questionViewController(for: question, answerCallback: answerCallback)
+        navigationController.pushViewController(viewController, animated: true)
     }
     
     func routeTo(result: QuizResult<String, String>) {
